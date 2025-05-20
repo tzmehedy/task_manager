1862f2e966c3aa7d753ad5b67f1005ad7f397b84
unknown <mehedytouhidurzaman@gmail.com>
2025-05-20 19:35:04 +0600

    final commit

--- task-manager/src/Components/TaskLists/TaskLists.jsx
+++ task-manager/src/Components/TaskLists/TaskLists.jsx
@@ -1,8 +1,12 @@
-import React from "react";
 import Taskitem from "../Taskitem/Taskitem";
 import "./TaskLists.css"
 
 const TaskLists = ({ taskLists, setTaskLists }) => {
+    if(taskLists.length === 0) return (
+      <div>
+        <p>No Task is remaining at this moment</p>
+      </div>
+    );
   return (
     <div id="task-list-container">
       {taskLists.map((taskList) => (
